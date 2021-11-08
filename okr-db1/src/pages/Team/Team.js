0ced7dcec0d1a7105a3b92c76f75cd3234effe3a
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-11-08 10:47:42 -0300

    feact[ Menu ] : insert team page  menu

--- okr-db1/src/pages/Team/Team.js
+++ okr-db1/src/pages/Team/Team.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Menu from '../../components/Menu/Menu';
 import "./Team.css";
 export default function Team(){
+    const optionsMenu = [
+        {
+            route:"/registergoals",
+            routeText:"New Quarter"
+        },
+        {
+            route:"/registerteampartner",
+            routeText:"New Team Partner"
+        }
+    ];
     return (
         <div className="team">
-           
+            <Menu options={optionsMenu}  hasLogo={true}/>
         </div>
     );
 }
