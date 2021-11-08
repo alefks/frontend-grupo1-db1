6ec700fcf444fba38632e3c50144eed1536b9341
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-11-08 01:05:18 -0300

    refactor[ TeamPartner ]: change from user employee

--- okr-db1/src/pages/RegisterTeamPartner/RegisterTeamPartner.js
+++ okr-db1/src/pages/RegisterTeamPartner/RegisterTeamPartner.js
@@ -1,12 +1,12 @@
 import React from "react";
-import './RegisterUserEmployee.css';
-import Title from './../../components/Title/Title';
-import Form from './../../components/Form/Form';
-import Input from './../../components/Input/Input';
-import Button from './../../components/Button/Button';
+import './RegisterTeamPartner.css';
+import Title from '../../components/Title/Title';
+import Form from '../../components/Form/Form';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
 import BoxButton from "../../components/BoxButton/BoxButton";
 
-export default function RegisterUserEmployee(){
+export default function RegisterTeamPartner(){
     return (
         <Form>
             <Title classname="title">New Team Partner</Title>
