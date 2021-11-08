6879d6502e0619fa59f568fa91e0e10adcd4072f
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-11-08 11:30:53 -0300

    feat[ Team ] : add box-team-Items

--- okr-db1/src/pages/Team/Team.js
+++ okr-db1/src/pages/Team/Team.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Box from '../../components/Box/Box';
 import Menu from '../../components/Menu/Menu';
+import TitleBox from '../../components/TitleBox/TitleBox';
 import "./Team.css";
 export default function Team(){
     const optionsMenu = [
         {
-            route:"/registergoals",
+            route:"/registerquarter",
             routeText:"New Quarter"
         },
         {
@@ -12,9 +14,29 @@ export default function Team(){
             routeText:"New Team Partner"
         }
     ];
+    const addQuarter = ()=>{
+        console.log("added new Quarter");
+    }
+    const addTeamPartner = ()=>{
+        console.log("added new team partner");
+    };
     return (
-        <div className="team">
+        <div className="team-body">
             <Menu options={optionsMenu}  hasLogo={true}/>
+            <Box classname="box-team-Items">
+                <Box classname="box-team-partner">
+                    <TitleBox classname="goals-name"> 
+                        Team Partners 
+                        <div className="add" onClick={addTeamPartner}>+</div>
+                    </TitleBox>
+                </Box>
+                <Box classname="box-quarters">
+                    <TitleBox classname="goals-name"> 
+                        Quarters 
+                        <div className="add" onClick={addQuarter}>+</div>
+                    </TitleBox>
+                </Box>
+            </Box>
         </div>
     );
 }
