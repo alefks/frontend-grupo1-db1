61cf43101750cf9661609263ef577c8dcebf37e4
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-11-08 11:40:52 -0300

    feact[ TeamPartner ] : create component

---
+++ okr-db1/src/components/TeamPartnerItem/TeamPartnerItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./TeamPartnerItem.css";
+export default function TeamPartnerItem(){
+    return (
+        <div className="team-partner">
+            team  partner
+        </div>
+    );
+}
