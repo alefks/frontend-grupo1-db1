08363a4cdc08e8897f6f3ae55b5cd7f0b27643f6
Rafael Souza <faelzsouza@gmail.com>
2021-11-09 14:12:32 -0300

    feat(fetchGetAllTeams): create a fetch to get all teams from db using useState and useEffect

--- okr-db1/src/pages/Home/Home.js
+++ okr-db1/src/pages/Home/Home.js
@@ -2,63 +2,77 @@ import React from "react";
 import ListTeams from "../../components/ListTeams/ListTeams";
 import Menu from "./../../components/Menu/Menu";
 import OptionsList from "../../components/OptionsList/OptionsList";
-import './Home.css';
+import "./Home.css";
+import { useEffect, useState } from "react/cjs/react.production.min";
+import { Api } from "../../api/api";
 
-export default function Home(){
-    const menuOptions = [{
-        route:"/registerteam/new",
-        routeText:"New Team"
-    },
+export default function Home() {
+    const [listTeams, setListTeams] = useState([]);
+    const menuOptions = [
+        {
+            route: "/registerteam/new",
+            routeText: "New Team",
+        },
     ];
-    const testListTeams=[
+
+    const fetchGetAllTeams = async () => {
+        const response = await Api.getAll("team");
+        const result = await response.json();
+        setListTeams(result);
+    };
+
+    useEffect(() => {
+        fetchGetAllTeams();
+    }, []);
+
+    const testListTeams = [
         {
             id: 1,
             departmentName: "financial",
-            totalValue: 30
+            totalValue: 30,
         },
         {
             id: 2,
             departmentName: "comercial",
-            totalValue: 60
+            totalValue: 60,
         },
         {
             id: 3,
             departmentName: "developer",
-            totalValue: 100
-        },{
+            totalValue: 100,
+        },
+        {
             id: 4,
             departmentName: "financial",
-            totalValue: 30
+            totalValue: 30,
         },
         {
             id: 5,
             departmentName: "comercial",
-            totalValue: 60
+            totalValue: 60,
         },
         {
             id: 6,
             departmentName: "developer",
-            totalValue: 100
+            totalValue: 100,
         },
-        
     ];
     const options = [
         {
-            name:"2020",
-            value:2020
-        },{
-            name:"2021",
-            value:2021
+            name: "2020",
+            value: 2020,
         },
-    ]
+        {
+            name: "2021",
+            value: 2021,
+        },
+    ];
     return (
         <div className="homeBody">
-            <Menu options={menuOptions} hasLogo={false} />            
+            <Menu options={menuOptions} hasLogo={false} />
             <ListTeams teams={testListTeams} year="2021">
-                <OptionsList options={options}>
-
-                </OptionsList>
+                <OptionsList options={options}></OptionsList>
             </ListTeams>
         </div>
-    )  ; 
+    );
 }
