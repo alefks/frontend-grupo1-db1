3503dd507a6f53014dbd21e1710a1ed8795f6128
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2021-11-15 18:15:20 -0300

    feat( CheckPoints ) : create page components

--- src/pages/CheckPoints/CheckPoints.js
+++ src/pages/CheckPoints/CheckPoints.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Box from "../../components/Box/Box";
+import Form from '../../components/Form/Form';
+import Button from '../../components/Button/Button';
 import './CheckPoints.css';
 
 export default function CheckPoints(){
     return (
-       <div className="check-points-body">
-           
+       <div className="body check-points">
+           <Form>
+                <Box classname="check-point-box">
+                    <div className="item-check">
+                        <label>item</label>
+                        <input></input>
+                    </div>
+                </Box>
+                <Box classname="column">
+                    <Box classname="note-block">
+                        <textarea className="note" ></textarea>
+                    </Box>    
+                    <Button>
+                        Save    
+                    </Button>        
+                </Box>
+            </Form>
        </div>
     );
 }
