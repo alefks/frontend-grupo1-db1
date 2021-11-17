6961a995289890201686ae5ad0386987e9b280af
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2021-11-17 18:53:20 -0300

    feat( Menu ) : mobile menu button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React,{ useState } from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
 import MenuRoutes from './MenuRoutes';
+import MenuIcon from './img/icons/menu.png';
 import './App.css';
 
 function App() {
+  const [showMenu,setShowMenu] = useState(true);
+  const openClose = ()=>{
+    const menu = document.getElementById('menu');
+    if(showMenu){
+      
+        menu.style="display:flex";
+        setShowMenu(false);
+    }else{
+        menu.style="display:none";
+        setShowMenu(true);
+    }
+  };
   return (
     <div className="App">
-
+      <div className="menu-button" onClick={openClose}>
+        <img src={MenuIcon} alt="menu-icon" />
+      </div>
       <Router >
         <MenuRoutes />
         <Routes/>
