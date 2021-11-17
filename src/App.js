0f68d237b3c56fc39622454181882efd16964595
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2021-11-16 21:36:08 -0300

    feat( css ) : add screen condition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
-import './App.css';
 import MenuRoutes from './MenuRoutes';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <Router >
         <MenuRoutes />
         <Routes/>
