1e875cb0d3e32c981ffa2a41f7ff4cf99c1da627
Rafael Souza <faelzsouza@gmail.com>
2021-11-18 11:24:36 -0300

    feat(fetchGetTeamPartnersList): fetch team partner list and patch a relation with team

--- src/components/AddPartner/AddPartner.js
+++ src/components/AddPartner/AddPartner.js
@@ -1,12 +1,16 @@
-import React from "react";
-import Form from './../Form/Form';
-import Button from './../Button/Button';
-import CancelLabel from '../CancelLabel/CancelLabel';
-import Title from '../Title/Title';
-import './AddPartner.css';
+import React, { useState, useEffect } from "react";
+import Form from "./../Form/Form";
+import Button from "./../Button/Button";
+import CancelLabel from "../CancelLabel/CancelLabel";
+import Title from "../Title/Title";
+import "./AddPartner.css";
+import Api from "../../api/api";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
 
-export default function AddPartner(props){
-    const partners = [
+export default function AddPartner(props) {
+    const { teamId } = useParams();
+
+    const partnersTestList = [
         {
             id: 1,
             name: "andreia",
@@ -15,21 +19,47 @@ export default function AddPartner(props){
             id: 2,
             name: "joao",
         },
-    ]
+    ];
+    const [partners, setPartners] = useState(partnersTestList);
+
+    const fetchGetTeamPartnersList = async () => {
+        const response = await Api.getAll("team-partner");
+        const result = await response.json();
+        setPartners(result);
+    };
+
+    useEffect(() => {
+        fetchGetTeamPartnersList();
+    }, []);
+
+    const getInputValues = async (event) => {
+        event.preventDefault();
+        const payload = {
+            teamPartners: +event.target.partner.value,
+        };
+
+        await Api.patch('team', teamId, payload)
+        
+        window.location.reload();
+    };
     return (
-        <Form classname={"form add"}>
+        <Form classname={"form add"} submitAction={getInputValues}>
             <Title classname="title">
                 Add new Team Partner
-                <CancelLabel action={props.closeButton} closeButton={true}/>
+                <CancelLabel action={props.closeButton} closeButton={true} />
             </Title>
             <select name="partner" id="cars" className="input">
-                {partners.map((value)=>(
-                    <option className="select-item" value={value.id} key={value.id}>{value.name}</option>
+                {partners.map((value) => (
+                    <option
+                        className="select-item"
+                        value={value.id}
+                        key={value.id}
+                    >
+                        {value.name}
+                    </option>
                 ))}
             </select>
-            <Button>
-                Add
-            </Button>
+            <Button>Add</Button>
         </Form>
-    )
+    );
 }
