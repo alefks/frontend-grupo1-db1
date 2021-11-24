b1336824f5097fe89a19a7c6a6805e9fb51712ba
Rafael Souza <faelzsouza@gmail.com>
2021-11-24 18:00:35 -0300

    feat(deleteItem): fetch to delete objective, keyresult or team-partner relation

--- src/components/DeleteItem/DeleteItem.js
+++ src/components/DeleteItem/DeleteItem.js
@@ -3,31 +3,32 @@ import Button from "../Button/Button";
 import Form from "../Form/Form";
 import Title from "../Title/Title";
 import CancelLabel from "../CancelLabel/CancelLabel";
-import './DeleteItem.css';
+import "./DeleteItem.css";
+import Api from "../../api/api";
 
-export default function DeleteItem(props){
-    const deleteItem =(event)=>{
+export default function DeleteItem(props) {
+    const deleteItem = async (event) => {
         event.preventDefault();
-        if(props.table === "objective"){
-            console.log("delted objective"+props.idItem);
-        }else if(props.table === "keyresult"){
-            console.log("delted keyresult "+props.idItem);
-        }else if(props.table === "partner"){
-            console.log(props.teamId);
-            console.log(props.idItem);
+        if (props.table === "objective") {
+            await Api.delete("objective", props.idItem);
+        } else if (props.table === "keyresult") {
+            await Api.delete("keyresult", props.idItem);
+        } else if (props.table === "partner") {
+            const payload = {
+                disconnectTeam: +props.teamId,
+            };
+            await Api.patch("team-partner", props.idItem, payload);
         }
-    }
-    return(
-        <Form submitAction={deleteItem} classname="form delete" >
+    };
+    return (
+        <Form submitAction={deleteItem} classname="form delete">
             <Title classname="title">
                 Are you really sure
-                <br/> 
+                <br />
                 about deleting this?
-                <CancelLabel closeButton={true} action={props.closeButton}/>
+                <CancelLabel closeButton={true} action={props.closeButton} />
             </Title>
-            <Button>
-                Confirm
-            </Button>
+            <Button>Confirm</Button>
         </Form>
     );
 }
