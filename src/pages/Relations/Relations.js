e36908c0033be3379a4d5b8eb4106bfe22ca108b
Rafael Souza <faelzsouza@gmail.com>
2021-11-26 14:32:16 -0300

    feat(fetchGetRelatedObjectives): fetch related objectives list

--- src/pages/Relations/Relations.js
+++ src/pages/Relations/Relations.js
@@ -1,10 +1,11 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Table from '../../components/Table/Table';
 import TableTitle from '../../components/TableTitle/TableTitle';
 import TableLine from '../../components/TableLine/TableLine';
 import { useParams } from "react-router-dom";
 import './Relations.css';
 import Box from '../../components/Box/Box';
+import Api from '../../api/api';
 
 export default function Relations(props){
     const {id} = useParams();
@@ -114,6 +115,21 @@ export default function Relations(props){
 
     ])
     const titles = lang.table.title;
+
+    const fetchGetRelatedObjectives = async () => {
+        const response = await Api.getById("objective/lista", id)
+        const result = await response.json()
+        const data = result.relatedObjectives.map(relatedItem => ({
+            id: relatedItem.id,
+            name: relatedItem.name,
+            team: relatedItem.team.name
+        }))
+        setRelations(data)
+    }
+
+    useEffect(() => {
+        fetchGetRelatedObjectives()
+    }, [])
 
     return (
         <div className="body relations">
